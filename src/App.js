d0731e1b3d2da82f7ac10e1c3c36c20c21489077
Reshma <reshmashaik0910@gmail.com>
2024-10-08 16:11:01 +0100

    changed

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import reactDom from 'react-dom'
+import ReactDOM from 'react-dom/client'
+const App = ()=>{
+    return(
+        <div><h1>Hello react</h1></div>
+    )
+}
+const root = reactDom.createRoot(document.getElementById('root'))
+root.render(<App/>)
